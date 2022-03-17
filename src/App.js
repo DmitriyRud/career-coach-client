d252f3112dcb754cb40b202efd3d74768643e2be
Dmitriy Rudakov <cska2004@gmail.com>
2022-03-17 13:29:52 +0300

    initial design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import 'antd/dist/antd.css'; 
+
+const { Header, Content, Footer } = Layout;
 // import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      privet privet
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            {new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+            })}
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     </div>
   );
 }
