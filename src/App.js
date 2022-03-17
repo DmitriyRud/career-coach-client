3279d764c4c501a594fec37f1d6507a5474217c3
Bender101 <serkok227@gmail.com>
2022-03-17 12:57:44 +0300

    create-client

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      privet
+    </div>
+  );
+}
+
+export default App;
