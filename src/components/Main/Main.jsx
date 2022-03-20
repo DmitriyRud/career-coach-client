8a7c63a1aede05722b3663631e7de3b2fa43e9f1
Dmitriy Rudakov <cska2004@gmail.com>
2022-03-20 12:14:43 +0300

    updated main page with texts and buttons

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,7 @@
 import './Main.css';
 import { useSelector } from 'react-redux'; 
 import { Button } from 'antd';
-import { DatabaseTwoTone } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { DatabaseTwoTone, FundTwoTone } from '@ant-design/icons';
 import { useNavigate } from "react-router-dom";
 
 const Main = () => {
@@ -12,21 +11,45 @@ const Main = () => {
 
   const skillsClickHandler = () => {
     navigate("users/profile/skills");
+  };
+
+  const analizeClickHandler = () => {
+    navigate("/search");
   }
 
   return ( 
     <div className="main-page">
       <h2>Привет, {userName}!</h2>
       <p>Я твой карьерный коуч. Ты сделал правильный выбор! Давай начнем строить твою карьеру!</p>
+      {/* Здесь нужно вставить проверку: если скилы не добавлены, выводить этот блок: */}
       <div className="skills-line">
         <p>Ты еще не добавил в свой профиль навыки. Давай это сделаем, чтобы я смог рекомендовать тебе лучшие вакансии:</p>
 
         <Button type="primary" shape="round" icon={<DatabaseTwoTone />} size={'large'} onClick={skillsClickHandler}>
           Заполнить таблицу навыков
         </Button>
+      </div>
+      {/* Если скилы добавлены, нужно вывести такой же блок, но с другим текстом и названием кнопки "Редактировать свои навыки" : */}
+      <div className="skills-line">
+      
+      </div>
+
+      {/* Здесь нужно вставить проверку: если анализ не проводился, выводить этот блок: */}
+      <div className="analize-line">
+        <p>Попробуй проанализировать рынок вакансий - это самый мощный мой инструмент. Проведя анализ, мы с тобой поймем, какие навыки требуются работодателям,
+          какие из них тебе нужно указать в резюме и портфолио, чтобы тебя точно взяли на работу!
+        </p>
+        <Button type="primary" shape="round" icon={<FundTwoTone />} size={'large'} onClick={analizeClickHandler}>
+          Начать анализ рынка вакансий
+        </Button>
+      </div>
+      {/* Если анализ проводился, нужно вывести такой же блок, но с другим текстом и названием кнопки "Провести новый анализ рынка вакансий" : */}
+      <div className="analize-line">
 
       </div>
-      <div className="searches-line">
+
+      {/* Если навыки добавлены и проводился хотя бы один анализ, нужно выводить в следующем блоке подходящие вакансии : */}
+      <div className="vacancies-line">
 
       </div>
     </div>
