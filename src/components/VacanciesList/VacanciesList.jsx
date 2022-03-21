60ae777bb9384625a8b9e4f7c69f9d269f03dc0d
Valera <vareraj@gmail.com>
2022-03-21 10:24:07 +0300

    init vacancies list

---
+++ src/components/VacanciesList/VacanciesList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './VacanciesList.css'
+
+const VacanciesList = () => {
+  return ( 
+    <div className="main-page">
+      <div className="vacancies-list-container">
+        <h1>
+          ТОП вакансий, подходящих по вашему запросу:
+        </h1>
+      </div>
+    </div>
+   );
+}
+ 
+export default VacanciesList;
