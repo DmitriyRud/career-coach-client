693495ded928be51ee99f053c8c537497270648d
Dmitriy Rudakov <cska2004@gmail.com>
2022-03-23 18:22:51 +0300

    added scrapper

--- src/redux/thunk/apiAC.js
+++ src/redux/thunk/apiAC.js
@@ -3,25 +3,27 @@ import { addRecomendation } from "../actions/recomendationAction";
 
 export const analizeAC = (data) => {
   return async (dispatch) => {
-    
-    if (data.method === 'api'){
-    // try {
-    //   const response = await fetch("/api/hh", {
-    //     method: "POST",
-    //     headers: { "Content-type": "application/json" },
-    //     body: JSON.stringify(data),
-    //   });
-    //   if (response.ok) {
-    //     const result = await response.json();
 
-    //     const resultId = result.resultId;
-    //     return resultId;
-    //   } else {
+    if (data.method === 'api') {
+      console.log('-----======= API selected =======-----');
+      console.log('data in analizeAC : ', data);
+      try {
+        const response = await fetch("/api/hh", {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(data),
+        });
+        if (response.ok) {
+          const result = await response.json();
 
-    //   }
-    // } catch (err) {
-    //   console.log("Error analize: ", err);
-    // }
+          const resultId = result.resultId;
+          return resultId;
+        } else {
+
+        }
+      } catch (err) {
+        console.log("Error analize: ", err);
+      }
     } else if (data.method === 'scrapping') {
       console.log('-----======= Scrapping selected =======-----');
       console.log('data in analizeAC : ', data);
@@ -33,7 +35,24 @@ export const analizeAC = (data) => {
       // title: "dd"
       // userId: 2
       // websites: (2) ['hh', 'habr']
+      try {
+        const response = await fetch("/scrap/hh", {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(data),
+        });
+        if (response.ok) {
+          const result = await response.json();
 
-    }
+          const resultId = result.resultId;
+          return resultId;
+        } else {
+
+        }
+      } catch (err) {
+        console.log("Error analize: ", err);
+
+      }
+    };
   };
 };
