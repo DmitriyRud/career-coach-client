c2f654d76b33f54593967a72ba07ebdff63a36d0
Dmitriy Rudakov <cska2004@gmail.com>
2022-03-24 15:02:13 +0300

    salary delimiter (space) added

--- src/components/VacanciesItem/VacanciesItem.jsx
+++ src/components/VacanciesItem/VacanciesItem.jsx
@@ -2,7 +2,13 @@ import React from "react";
 // import { Button } from 'antd';
 import './VacanciesItem.css'
 
+function replacer(match, p1, p2, offset, string) {
+  // p1 is nondigits, p2 digits, and p3 non-alphanumerics
+  return [p1, p2].join(' ');
+}
+
 const VacanciesItem = ({ title, company, url, salary }) => {
+  salary = salary.replace(/(\d+)(\d{3,})/gm, replacer);
   return (
     <div className="vacancy-div">
       <div className="vacancy-title">
